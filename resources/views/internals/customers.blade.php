202b3202702f4b083cf13bce167e16fbbed236ed
tostos <saittosun2@gmail.com>
2020-12-31 16:41:07 +0100

    eloquent where clause

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -23,15 +23,35 @@
         {{ $errors->first('email')}}
       </div>
     </div>
+    <div class="form-group">
+      <label for="active" class="col-form-label">Status</label>
+      <select name="active" id="active" class="custom-select custom-select-sm">
+        <option value="" disabled>Select custumer status</option>
+        <option value="1">Active</option>
+        <option value="0">Inactive</option>
+      </select>
+    </div>
     <button type="submit" class="btn btn-primary my-3">Add Customer</button>
 
     @csrf
   </form>
 
-
-  <ul>
-      @foreach ($customers as $customer)
-        <li>{{ $customer->name }} <span class="text-muted">({{$customer->email}})</span></li>
-      @endforeach
-  </ul>
+  <div class="row">
+    <div class="col-6">   
+      <h3>Active Customers</h3> 
+      <ul>
+          @foreach ($activeCustomers as $activeCustomer)
+            <li>{{ $activeCustomer->name }} <span class="text-muted">({{$activeCustomer->email}})</span></li>
+          @endforeach
+      </ul>
+    </div>
+    <div class="col-6"> 
+      <h3>In-Active Customers</h3> 
+      <ul>
+          @foreach ($inactiveCustomers as $inactiveCustomer)
+            <li>{{ $inactiveCustomer->name }} <span class="text-muted">({{$inactiveCustomer->email}})</span></li>
+          @endforeach
+      </ul>
+    </div>
+  </divC
 @endsection
